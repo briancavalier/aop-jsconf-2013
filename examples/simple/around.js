ca63a1b3d5c73728cc019ce163109b3f4e00c51f
Brian Cavalier <brian@hovercraftstudios.com>
2013-05-21 16:46:12 -0400

    Add text to around examples

--- examples/simple/around.js
+++ examples/simple/around.js
@@ -14,6 +14,22 @@ function fibonacci(n) {
 		: fibonacci(n-1) + fibonacci(n - 2);
 }
 
+section([
+	'Yay, fibonacci, everyone\'s fav',
+	'But wait, we can\'t see it doing anything'
+]);
+
+fibonacci(0);
+fibonacci(1);
+fibonacci(2);
+fibonacci(3);
+fibonacci(4);
+fibonacci(5);
+
+section([
+	'Let\'s add some simple logging'
+]);
+
 fib = aop.afterReturning(fibonacci, console.log.bind(console));
 
 fib(0);
@@ -22,6 +38,11 @@ fib(2);
 fib(3);
 fib(4);
 fib(5);
+
+section([
+	'That\'s better, but it\'d be nicer if we could see the input',
+	'and the output.  Around advice can do that.'
+]);
 
 fib = aop.around(fibonacci, function(orig, args) {
 	var result = orig.apply(this, args);
@@ -37,6 +58,10 @@ fib(2);
 fib(3);
 fib(4);
 fib(5);
+
+section([
+	'Cool, and now we can wrap that up into a prettyPrint "aspect"'
+]);
 
 function prettyPrint(f) {
 	return aop.around(f, function(orig, args) {
@@ -57,6 +82,13 @@ fib(3);
 fib(4);
 fib(5);
 
+section([
+	'Fibonacci is computationally intensive, so let\'s profile it',
+	'to see how it behaves with bigger inputs, and since we already',
+	'have a prettyPrint aspect, let\'s keep using that, too'
+]);
+
+
 function profile(f) {
 	return aop.around(f, function(orig, args) {
 		var start = Date.now();
@@ -74,6 +106,13 @@ fib(10);
 fib(20);
 fib(30);
 fib(40);
+
+section([
+	'Wow, things get brutal around 40.  Let\'s memoize it so we',
+	'only pay that cost once.  We can stack prettyPrint and profiling',
+	'with memoization too.',
+	'Pretty printed, profiled, memoized fibonacci, here we go:'
+]);
 
 function memoize(f) {
 	var cache = {};
