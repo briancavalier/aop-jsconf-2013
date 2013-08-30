0265609b28a5905bd72874e96b19f39f54966a79
Brian Cavalier <brian@hovercraftstudios.com>
2013-08-30 12:13:17 -0400

    Update for bower 1.x

--- demo-app/run.js
+++ demo-app/run.js
@@ -17,12 +17,12 @@
 //		paths: { 'model/Cart': 'common/model/CartFail' },
 		// AMD packages. Note we're loading aop-simple as pure CommonJS
 		packages: {
-			curl:  { location: '../components/curl/src/curl' },
-			when:  { location: '../components/when', main: 'when' },
-			meld:  { location: '../components/meld', main: 'meld' },
+			curl:  { location: '../bower_components/curl/src/curl' },
+			when:  { location: '../bower_components/when', main: 'when' },
+			meld:  { location: '../bower_components/meld', main: 'meld' },
 			aop:   { location: '../src', main: 'aop-simple',
 				config: { moduleLoader: 'curl/loader/cjsm11' }},
-			wire:  { location: '../components/wire', main: 'wire' },
+			wire:  { location: '../bower_components/wire', main: 'wire' },
 			model: { location: 'common/model' },
 			dom:   { location: 'common/dom' },
 			data:  { location: 'common/data' },
